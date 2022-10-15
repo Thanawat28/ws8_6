d7449fabbde7b92817f9bb69cb277ac10c5e77b1
Thanawat28 <109870899+Thanawat28@users.noreply.github.com>
2022-10-15 14:54:56 +0700

    Delete workshop6 directory

--- workshop6/delete.php
+++
@@ -1,8 +0,0 @@
-<?php include "connect.php" ?>
-
-<?php
-$stmt = $pdo->prepare("DELETE FROM member WHERE username=?");
-$stmt->bindParam(1, $_GET["username"]); 
-if ($stmt->execute()) 
-header("location: list-member.php"); 
-?>
